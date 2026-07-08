a85505abc3ce80fed6aa4b79a917cc0c3fda9a06
bigdale123 <dylcal138@gmail.com>
2026-07-08 16:24:34 -0500

    board change, should work for mono

--- wordle.js
+++ wordle.js
@@ -199,6 +199,13 @@ function generate_intro_card(){
     intro_page_lines.push(centerText("",40));
     intro_page_lines.push(centerText("Welcome to Wordle!",40));
     intro_page_lines.push(centerText("Guess the " + WORD_LENGTH + "-letter word in " + MAX_ATTEMPTS + " tries.", 40));
+    intro_page_lines.push(centerText("",40));
+    intro_page_lines.push(centerText("",40));
+    intro_page_lines.push(centerText(CTRL_A + "K" + CTRL_A + "2" + "(X)" + CTRL_A + "N" + " = Correct"), 40);
+    intro_page_lines.push(centerText("",40));
+    intro_page_lines.push(centerText(CTRL_A + "K" + CTRL_A + "3" + "!X!" + CTRL_A + "N" + " = Misplaced"), 40);
+    intro_page_lines.push(centerText("",40));
+    intro_page_lines.push(centerText(CTRL_A + "K" + CTRL_A + "7" + " X " + CTRL_A + "N" + " = Not in word"), 40);
 
     return intro_page_lines;
 }
@@ -525,17 +532,7 @@ function startWordle(mode) {
         }
         else {
             console.printfile(js.exec_dir + "banner.msg"); // 13 Rows
-            var intro_page_lines = [];
-            intro_page_lines.push(centerText("",40));
-            intro_page_lines.push(centerText("Welcome to Wordle!",40));
-            intro_page_lines.push(centerText("Guess the " + WORD_LENGTH + "-letter word in " + MAX_ATTEMPTS + " tries.", 40));
-            intro_page_lines.push(centerText("",40));
-            intro_page_lines.push(centerText("",40));
-            intro_page_lines.push(centerText(CTRL_A + "K" + CTRL_A + "2" + "(X)" + CTRL_A + "N" + " = Correct"), 40);
-            intro_page_lines.push(centerText("",40));
-            intro_page_lines.push(centerText(CTRL_A + "K" + CTRL_A + "3" + "!X!" + CTRL_A + "N" + " = Misplaced"));
-            intro_page_lines.push(centerText("",40));
-            intro_page_lines.push(centerText(CTRL_A + "K" + CTRL_A + "7" + " X " + CTRL_A + "N" + " = Not in word"));
+            var intro_page_lines = generate_intro_card();
             var scoreboard_lines = generate_scoreboard(5, mode);
             console.putmsg("\n", p_mode=P_NOPAUSE);
             for (i = 0; i < scoreboard_lines.length; i++) {
